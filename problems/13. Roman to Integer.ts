4fb89f5e5810dd89018d0977fcbb4842a2d6d282
Hanif Sheikh <hanifsheikh.mail@gmail.com>
2023-06-20 03:03:11 +0600

    Roman to Integer

---
+++ problems/13. Roman to Integer.ts
@@ -0,0 +1,56 @@
+
+// https://leetcode.com/problems/roman-to-integer/
+// Constraints:
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+enum RomanChars {
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000
+}
+
+const romanToInt = (s: string): number => {
+
+    let value = 0;
+
+    for(let i = 0; i < s.length; i++){
+        const char = s[i]       
+        if(!Object.keys(RomanChars).includes(char)){
+            throw new Error("Input Character '"+ char +"' is not a valid roman character.")
+        }
+        else {
+           const charValue = RomanChars[char as keyof typeof RomanChars]
+           if(i > 0){
+           const prevCharValue = RomanChars[s[i-1] as keyof typeof RomanChars]
+           value += ( charValue > prevCharValue ? (charValue - (prevCharValue * 2)) : charValue )          
+        }
+        else {
+            value += charValue            
+           }
+        }
+    }    
+    if(value > 3999){
+        throw new Error("Range is 1 to 3999");
+    }
+    return value
+};
+
+export default function main(){
+
+    const s = "MCMXCIV"    
+
+    if(1 <= s.length && s.length <= 15){
+        const result = romanToInt(s)
+        console.log(`Roman Input: '${s}'\nOutput: ${result}`)
+    }
+
+    else {
+        throw new Error("Input string can not be empty or more than 15 characters.")
+    }
+}
